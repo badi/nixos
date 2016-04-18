4fb7633454661d65ba543cbca42ddf1561b91319
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-04-18 15:43:13 -0400

    gambitNG: record machine-id

--- gambitNG.nix
+++ gambitNG.nix
@@ -11,6 +11,8 @@
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/disk/by-id/ata-Crucial_CT1024MX200SSD1_15030E747E7E";
 
+  # /etc/machine-id
+  # '16-04-18: 2f610599c12a424a80bd5b88fa8cf426
   networking.hostId = "6c7cbbf3";
   networking.hostName = "gambit";
 
