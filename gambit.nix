7ce87d59cad9d56c1db20f92a7db392abe35cfed
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-05-25 15:26:18 -0400

    migrate gambit to NG

--- gambit.nix
+++ gambit.nix
@@ -4,16 +4,22 @@
 
   imports = [
     ./gambit/hardware-configuration.nix
+    ./basicSystem.nix
+    ./workstation.nix
     ./desktopManager.nix
+    ./users.nix
+    ./yubikey.nix
+    ./monitoring.nix
+    ./virthost.nix
   ];
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/disk/by-id/ata-Crucial_CT1024MX200SSD1_15030E747E7E";
 
+  # /etc/machine-id
+  # '16-04-18: 2f610599c12a424a80bd5b88fa8cf426
   networking.hostId = "6c7cbbf3";
   networking.hostName = "gambit";
 
-  # services.xserver.videoDrivers = [ "nvidia" ];
-  # hardware.opengl.driSupport32Bit = true;
 }
