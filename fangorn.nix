2a15a032057b533067ab6021f23bf8d5b3b4aee0
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-05-24 23:44:27 -0400

    migrate fangorn to NG

--- fangorn.nix
+++ fangorn.nix
@@ -2,15 +2,28 @@
 
 {
 
-  imports = [ ./fangorn/hardware-configuration.nix ];
+  imports = [
+    ./fangorn/hardware-configuration.nix
+    ./basicSystem.nix
+    ./workstation.nix
+    ./desktopManager.nix
+    ./users.nix
+    ./yubikey.nix
+    ./monitoring.nix
+  ];
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "/dev/disk/by-id/ata-Samsung_SSD_840_EVO_250GB_S1DBNSAF757040Z";
 
   networking.hostId = "f125f099";
   networking.hostName = "fangorn"; # Define your hostname.
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.driSupport32Bit = true;
+
+  environment.systemPackages = with pkgs; [
+    smbclient cifs_utils
+  ];
+
 }
