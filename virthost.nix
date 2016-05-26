095bbab1455913a66ac7c3c8c52994e47b9cdb94
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-05-26 12:54:31 -0400

    add docker as virtualization option

--- virthost.nix
+++ virthost.nix
@@ -3,6 +3,7 @@
 {
 
   boot.initrd.availableKernelModules = ["tun" "kvm-intel" "virtio"];
+  virtualisation.docker.enable       = true;
   virtualisation.libvirtd.enable     = true;
   virtualisation.libvirtd.enableKVM  = true;
   environment.systemPackages         = with pkgs; [ virtmanager ];
