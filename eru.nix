1d3d6f490b017431cd53539b59c3c019ad72a20d
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-09-27 19:55:47 +0000

    eru: add ssh key for fangorn (nopass)

--- eru.nix
+++ eru.nix
@@ -32,6 +32,8 @@
     openssh.authorizedKeys.keys = [
       # badi@fangorn
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINddUe+ma6S0Y27La0wGd5JVSiVwiza4Xal5dtYub0x0 badi@fangorn abdulwahidc@gmail.com"
+      # badi@fangorn nopass
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2StA8q/t+vblYXdLf0woNYv8akBhaH/P//BMwC9UtO badi@fangorn"
       # badi@gambit
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqoNyVmzGibqaa0rTLN5OdrVlczLrEULHJdynesL9eIOcJujYkmfZxPfd/lun9T8eopjpODVMiBqjQI8Sf9S76lgihY5c2KQcXMj+S6OgNFQJa8ve7e8cUS2KQtrcPpMcmpm62/5kB7CYP1I61TfLQfPSkoxkQ408hZ4sj1O4xYujbZIaWlSokcedoh7HjIkRcqUeeoxspyxPV9ftaFuCFcXVlDC7wSe9+SoeTmU8BygmgpAy19nezf24VDe1dVnpOMDqB7Qqa5nXABv5ZwUXMlU1uw5fPw05DkCB0bc5TqaMHcwT/U9IwSJunRXepXIFfvnBakScUGkq9Uld0RpJT"
     ];
