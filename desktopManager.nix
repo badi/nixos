d7e9554d4f5b2c7ef8721d36b4720531224657a5
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2017-07-13 16:25:31 -0400

    save desktopManager

--- desktopManager.nix
+++ desktopManager.nix
@@ -37,8 +37,9 @@ in
   hardware.opengl.enable = true;
   hardware.opengl.driSupport = true;
 
-  services.xserver.desktopManager.gnome3.sessionPath = with pkgs; [ clearlooks-phenix theme-vertex ];
-  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome3.sessionPath = with pkgs; [ clearlooks-phenix theme-vertex ];
+  services.xserver.desktopManager.kde5.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
   services.xserver.windowManager.xmonad = {
     enable = true;
     enableContribAndExtras = true;
@@ -68,30 +69,30 @@ in
     pavucontrol
     clearlooks-phenix
     theme-vertex
-    gnome3.gnome-backgrounds
-    gnome3.gnome_control_center
-    gnome3.gnome_keyring
-    gnome3.gnome_session
-    gnome3.gnome_settings_daemon
-    gnome3.gnome_themes_standard
+    # gnome3.gnome-backgrounds
+    # gnome3.gnome_control_center
+    # gnome3.gnome_keyring
+    # gnome3.gnome_session
+    # gnome3.gnome_settings_daemon
+    # gnome3.gnome_themes_standard
     mesa
     mesa_drivers
   ];
 
-  services.gnome3 = {
-    at-spi2-core.enable = true;
-    # evolution-data-server.enable = true;
-    # gnome-documents.enable = true;
-    gnome-keyring.enable = true;
-    gvfs.enable = true;
-    sushi.enable = true;
-    tracker.enable = false;
-  };
+  # services.gnome3 = {
+  #   at-spi2-core.enable = true;
+  #   # evolution-data-server.enable = true;
+  #   # gnome-documents.enable = true;
+  #   gnome-keyring.enable = true;
+  #   gvfs.enable = true;
+  #   sushi.enable = true;
+  #   tracker.enable = false;
+  # };
 
-  services.accounts-daemon.enable = true;
+  # services.accounts-daemon.enable = true;
   services.devmon.enable = true;
-  services.geoclue2.enable = true;
-  services.telepathy.enable = true;
-  services.udisks2.enable = true;
+  # services.geoclue2.enable = true;
+  # services.telepathy.enable = true;
+  # services.udisks2.enable = true;
 
 }
