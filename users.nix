af6fd6e69387d2c869e6c361d316c0a655035f19
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2017-07-13 16:25:43 -0400

    add test user

--- users.nix
+++ users.nix
@@ -1,5 +1,12 @@
 { ... }:
 {
+  users.extraUsers.test = {
+    isNormalUser = true;
+    createHome = true;
+    extraGroups = [ "wheel" ];
+    initialHashedPassword = "$6$uANNctji62$bG271HHKiDMP8bv9cOyP7OoXuFwexMQapXFmkhKMAsZ.0EM8UowCuckxNg20TQRFBvxhtL3U9s4bB7xzg0vw60";
+  };
+
   users.extraUsers.badi = {
     isNormalUser = true;
     initialHashedPassword = "$6$.YFrPdd1$/nViiUzPHuKOUYwDd3hZdG6HVm9zUOp49cv4bJkZ/InUop97mQ8HT3l7TlCWJpbN0rL0x7weVHZlSTNl0rBA11";
