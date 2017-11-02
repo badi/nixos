bc0d859e4dcc9a51bda4f20267162ced0b7ba69e
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2017-11-01 21:25:09 -0400

    desktop manager: simplify/cleanup

--- common/desktopManager.nix
+++ common/desktopManager.nix
@@ -1,49 +1,13 @@
 { pkgs, config, ... }:
 
-let
-
-  all-the-icons-fn =
-    {stdenv, fetchFromGitHub}:
-
-    stdenv.mkDerivation {
-      name = "all-the-icons";
-
-      src = fetchFromGitHub {
-        owner = "domtronn";
-        repo = "all-the-icons.el";
-        rev = "b2d923e51d23e84198e21b025c656bf862eaced6";
-        sha256 = "0j5230nas9h6rn4wfsaf5pgd3yxxk615j68y2j01pjrrkxvrwqig";
-      };
-
-      dontBuild = true;
-      phases = [ "installPhase" ];
-      installPhase = ''
-
-        fontDir=$out/share/fonts/truetype
-        mkdir -p $fontDir
-        cp $src/fonts/*.ttf $fontDir
-      '';
-
-      meta = {
-        platforms = stdenv.lib.platforms.unix;
-      };
-    };
-
-  all-the-icons = pkgs.callPackage all-the-icons-fn {};
-
-in
 {
 
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.package = pkgs.pulseaudioFull;
+  hardware.pulseaudio.daemon.config.flat-volumes = "no";
 
-  # services.xserver.desktopManager.gnome3.sessionPath = with pkgs; [ clearlooks-phenix theme-vertex ];
-  services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.windowManager.xmonad = {
-    enable = true;
-    enableContribAndExtras = true;
-  };
+  services.xserver.desktopManager.plasma5.enable = true;
 
   fonts = {
     enableDefaultFonts = true;
@@ -60,40 +24,7 @@ in
       terminus_font
       ubuntu_font_family
       unifont
-      all-the-icons
     ];
   };
-
-  # environment.systemPackages = with pkgs; [
-  #   gtk2
-  #   gtk3
-  #   pavucontrol
-  #   clearlooks-phenix
-  #   # theme-vertex
-  #   # gnome3.gnome-backgrounds
-  #   # gnome3.gnome_control_center
-  #   # gnome3.gnome_keyring
-  #   # gnome3.gnome_session
-  #   # gnome3.gnome_settings_daemon
-  #   # gnome3.gnome_themes_standard
-  #   mesa
-  #   mesa_drivers
-  # ];
-
-  # services.gnome3 = {
-  #   at-spi2-core.enable = true;
-  #   # evolution-data-server.enable = true;
-  #   # gnome-documents.enable = true;
-  #   gnome-keyring.enable = true;
-  #   gvfs.enable = true;
-  #   sushi.enable = true;
-  #   tracker.enable = false;
-  # };
-
-  # services.accounts-daemon.enable = true;
-  services.devmon.enable = true;
-  # services.geoclue2.enable = true;
-  # services.telepathy.enable = true;
-  # services.udisks2.enable = true;
 
 }
