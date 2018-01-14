3a1c26350670abd8a830506a8b5e037dd94ea9ce
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2018-01-14 07:34:38 -0500

    make firefox overlay available as pkgs.firefox-overlay

--- common/firefox-nightly.nix
+++ common/firefox-nightly.nix
@@ -6,7 +6,7 @@ let
 	  sha256 = "082a38nnwabr8ilpk7myvql0dcwyr39gw5ypqxml53z5kl0lxib5";
   };
 
-  firefox-overlay = import "${nixpkgs-mozilla}/firefox-overlay.nix";
+  overlay = import "${nixpkgs-mozilla}/firefox-overlay.nix";
 
 in
 
@@ -14,13 +14,14 @@ in
 
   nixpkgs.overlays = [
     (self: super: {
-      inherit ((firefox-overlay self super).latest) firefox-nightly-bin;
+      firefox-overlay = (overlay self super).latest;
+      # inherit ((firefox-overlay self super).latest) firefox-nightly-bin;
     })
   ];
 
 
   environment.systemPackages = with pkgs; [
-    firefox-nightly-bin
+    firefox-overlay.firefox-nightly-bin
   ];
 
 }
