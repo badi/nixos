4ed606f1a25b17782a2c724dfd0056b2226d135f
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2019-01-19 18:40:57 -0600

    junkblocker: update

--- common/junk-blocker.nix
+++ common/junk-blocker.nix
@@ -4,7 +4,7 @@
 
   networking.extraHosts = builtins.readFile (pkgs.fetchurl {
     url = https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts;
-    sha256 = "0755vpzhy0wcgc0lpfcmbkmbdrm42x6w5znlyjbdfxr6lkbpqp0s";
+    sha256 = "01bhx494mpg5hb989fkxcaggl8xl4ca5wkqya7imb0x8amyi7453";
   });
 
 }
