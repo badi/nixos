b2c86cb6998e7beb3b17a95c9edf663ef4ee7276
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2019-01-27 00:37:40 -0600

    fangorn: add taffybar, status-notifier-watcher, nm-applet, pa-applet

--- fangorn/systemd-user.nix
+++ fangorn/systemd-user.nix
@@ -29,13 +29,61 @@
     '';
   };
 
-  systemd.user.services = {
-    parcellite = {
+  systemd.user.targets = {
+    wm = {
       enable = true;
-      description = "Parcellite: ${pkgs.parcellite.meta.description}";
-      serviceConfig = {
-        ExecStart = "${pkgs.parcellite}/bin/parcellite";
+      description = "Window Manager";
+      unitConfig = {
+        AllowIsolate = "yes";
       };
+    };
+  };
+  systemd.user.services =
+    let
+      mk-service = {name, pkg, bin-path ? null, env ? {}, depends-on ? [], wantedBy ? [ "wm.target" ]}:
+      let
+        bin = if isNull bin-path then "bin/${name}" else bin-path;
+      in
+      { enable = true;
+        description = "${name}";
+        serviceConfig = {
+          ExecStart = "${pkg}/${bin}";
+        };
+        unitConfig = {
+          Wants = depends-on;
+          After = depends-on;
+        };
+        inherit wantedBy;
+        environment = env;
+      };
+    in
+  {
+    taffybar = mk-service {
+      name = "taffybar";
+      pkg = pkgs.taffybar;
+      depends-on = [ "status-notifier-watcher.service" ];
+      env = {
+        GDK_PIXBUF_MODULE_FILE = "${pkgs.librsvg}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache";
+      };
+    };
+    status-notifier-watcher = mk-service {
+      name = "status-notifier-watcher";
+      pkg = pkgs.haskellPackages.status-notifier-item;
+    };
+    parcellite = mk-service {
+      name = "parcellite";
+      pkg = pkgs.parcellite;
+      depends-on = [ "taffybar.service" ];
+    };
+    nm-applet = mk-service {
+      name = "nm-applet";
+      pkg = pkgs.networkmanagerapplet;
+      depends-on = [ "taffybar.service" ];
+    };
+    pa-applet = mk-service {
+      name = "pa-applet";
+      pkg = pkgs.pa_applet;
+      depends-on = [ "taffybar.service" ];
     };
   };
 
