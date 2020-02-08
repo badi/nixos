7ab3c0c3360fd106e186eb4952364009c7faa9c1
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2020-02-08 09:29:53 -0600

    namo: save

--- namo/boot.nix
+++ namo/boot.nix
@@ -1,9 +1,5 @@
 { config, pkgs, ... }:
 
-let
-  secrets = import ../secrets {};
-in
-
 {
 
   imports = [ ./hardware-configuration.nix ];
@@ -39,26 +35,6 @@ in
   services.zfs.autoScrub.enable = true;
   services.zfs.autoScrub.interval = "weekly";
   services.zfs.autoScrub.pools = [ "mandos" ];
-
-  services.prometheus.exporters.node.enable = true;
-  services.prometheus.exporters.node.openFirewall = true;
-
-  users.extraGroups.mandos.gid = 1002;
-  users.extraUsers.badi = {
-    isNormalUser = true;
-    uid = 1001;
-    createHome = true;
-    extraGroups = [ "wheel" "mandos"];
-    initialHashedPassword = "$6$57MWbD8YW3YKQsI$UUyMnkH0k8WDJ7vOt3cgkj8XVO7QBsVeR79GyqydEFmKtqAEg1n9y853BGgzc0QksNnlrUreguWxDsYrYipQU/";
-    openssh.authorizedKeys.keys =
-      secrets.ssh-keys.badi.fangorn;
-  };
-
-
-
-  i18n.consoleFont = "lat2-Terminus16";
-  i18n.consoleKeyMap = "us";
-  i18n.defaultLocale = "en_US.UTF-8";
 
   swapDevices = [ { device = "/dev/disk/by-id/ata-WDC_WD1001FALS-00J7B1_WD-WMATV1264214-part1"; } ];
 
--- namo/default.nix
+++ namo/default.nix
@@ -1,5 +1,9 @@
 { config, pkgs, lib, ... }:
 
+let
+  secrets = import ../secrets {};
+in
+
 {
   imports = [
     ../common/basicSystem.nix
@@ -33,10 +37,31 @@
 
   services.smartd.enable = true;
 
+  services.prometheus.exporters.node.enable = true;
+  services.prometheus.exporters.node.openFirewall = true;
+
   services.syncthing.enable = true;
   services.syncthing.openDefaultPorts = true;
 
   sound.enable = false;
+
+  users.extraGroups.mandos.gid = 1002;
+  users.extraUsers.badi = {
+    isNormalUser = true;
+    uid = 1001;
+    createHome = true;
+    extraGroups = [ "wheel" "mandos"];
+    initialHashedPassword = "$6$57MWbD8YW3YKQsI$UUyMnkH0k8WDJ7vOt3cgkj8XVO7QBsVeR79GyqydEFmKtqAEg1n9y853BGgzc0QksNnlrUreguWxDsYrYipQU/";
+    openssh.authorizedKeys.keys =
+      secrets.ssh-keys.badi.fangorn;
+  };
+
+
+
+  i18n.consoleFont = "lat2-Terminus16";
+  i18n.consoleKeyMap = "us";
+  i18n.defaultLocale = "en_US.UTF-8";
+
 
   system.stateVersion = "16.09";
 
