192fd3a1a6264481e1e1e8a44a23542e5d4bef3c
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2020-03-27 17:04:38 -0500

    nole is terminated

--- network/default.nix
+++ network/default.nix
@@ -34,21 +34,22 @@ in
     imports = [ ../namo ];
   };
 
-  nole = {resources, lib, ...}: {
-    deployment.targetEnv = "ec2";
-    imports = [ ../nole ];
-    deployment.ec2 =
-      let
-        tf = (lib.head (lib.importJSON ../terraform/prod/terraform.tfstate).modules).resources;
-    in
-    {
-      inherit (secrets.aws) accessKeyId region;
-      inherit (secrets.nole) instanceType ebsOptimized ebsInitialRootDiskSize usePrivateIpAddress;
-      subnetId = tf."aws_subnet.main".primary.id;
-      securityGroupIds = [ tf."aws_security_group.allow_all".primary.id ];
-      elasticIPv4 = tf."aws_eip.main".primary.attributes.public_ip;
-      keyPair = resources.ec2KeyPairs.keypair;
-    };
-  };
+  # terminated
+  # nole = {resources, lib, ...}: {
+  #   deployment.targetEnv = "ec2";
+  #   imports = [ ../nole ];
+  #   deployment.ec2 =
+  #     let
+  #       tf = (lib.head (lib.importJSON ../terraform/prod/terraform.tfstate).modules).resources;
+  #   in
+  #   {
+  #     inherit (secrets.aws) accessKeyId region;
+  #     inherit (secrets.nole) instanceType ebsOptimized ebsInitialRootDiskSize usePrivateIpAddress;
+  #     subnetId = tf."aws_subnet.main".primary.id;
+  #     securityGroupIds = [ tf."aws_security_group.allow_all".primary.id ];
+  #     elasticIPv4 = tf."aws_eip.main".primary.attributes.public_ip;
+  #     keyPair = resources.ec2KeyPairs.keypair;
+  #   };
+  # };
 
 }
